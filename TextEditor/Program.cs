0d6dee21b64ba3254c9e7d7b59039159a4bac129
Andre Oliveira <79090202+AndrewOliver616@users.noreply.github.com>
2022-02-09 14:36:50 -0300

    Adicionar arquivos de projeto.

---
+++ TextEditor/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Menu() 
+        {
+            Console.Clear();
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1 - Open file");
+            Console.WriteLine("2 - Edit file");
+            Console.WriteLine("0 - Exit");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1: Open();break;
+                case 2: Edit();break;
+                case 0: System.Environment.Exit(0);break;
+                default: Menu(); break;
+
+            }
+        }
+
+        static void Open() 
+        {
+            Console.Clear();
+            Console.WriteLine("What's the file path?");
+            string path = Console.ReadLine();
+
+            using(var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Edit() { }
+
+        static void Save() { }
+    }
+}
